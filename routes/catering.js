6da76bdb1c0b783ac6945081dd06fbb73dff1d60
Joachim Alexandre Jasmin <joachim.jasmin-dev@proton.me>
2025-06-24 19:42:04 +0200

    Modification de la route pour récupérer toutes les entreprises de traiteur : ajout de la pagination et des champs supplémentaires pour une meilleure gestion des données.

--- routes/catering.js
+++ routes/catering.js
@@ -4,10 +4,23 @@ var router = express.Router();
 const Venue = require("../models/Venue");
 
 // GET all catering companies
-router.get("/", async (req, res) => {
+router.get("/all/page", async (req, res) => {
+	const { page = 1 } = req.query;
+	const skip = (page - 1) * 10;
+
 	try {
-		const cateringCompanies = await Venue.find().select("name url");
-		res.status(200).json({ cateringCompanies });
+		const cateringCompanies = await Venue.find()
+			.skip(skip)
+			.select("name imageUrl url averageRating location aiGlobalScore aiKeyPoints aiSummary")
+			.lean();
+			
+		const formatted = cateringCompanies.map((c) => ({
+			...c,
+			city: !c?.location.region ? "N/A" : c?.location.city,
+			region: c?.location.region ?? c?.location.city,
+		}));
+
+		res.status(200).json({ companies: formatted });
 	} catch (error) {
 		console.error("Error fetching catering companies:", error);
 		res.status(500).json({ message: "Internal server error" });
